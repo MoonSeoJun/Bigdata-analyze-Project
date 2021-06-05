9a6dd84e01dd412128bccd6c451c80da31a93228
MoonSeoJun <msj00130@gmail.com>
2021-06-05 10:13:50 +0900

    add : add four leagues

--- club_balance.py
+++ club_balance.py
@@ -2,7 +2,7 @@ import requests
 from bs4 import BeautifulSoup
 
 # 리그 내에서 발생하는 지출액과 수익을 수집하기 위한 함수
-def get_club_expenditure_few_year(start_year, end_year, team_num):
+def get_club_expenditure_few_year(start_year, end_year, team_num, league_num):
     total_clubs_info = []
     for_return_club_info = []
 
@@ -14,8 +14,13 @@ def get_club_expenditure_few_year(start_year, end_year, team_num):
             'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36'}
 
     # 리그에서 시작 연도와 끝 연도 동안 지출한 금액과 수익을 받아오기 위한 URL
-    url = fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=189&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0'
-    req = requests.get(url, headers=headers)
+    url = [fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=189&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0',
+    fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=75&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0',
+    fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=40&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0',
+    fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=50&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0',
+    fr'https://www.transfermarkt.com/transfers/einnahmenausgaben/statistik/plus/0?ids=a&sa=&saison_id={start_year}&saison_id_bis={end_year}&land_id=157&nat=&pos=&altersklasse=&w_s=&leihe=&intern=0']
+
+    req = requests.get(url[league_num], headers=headers)
 
     if req.status_code == requests.codes.ok:
             soup = BeautifulSoup(req.text, 'lxml')
@@ -33,15 +38,18 @@ def get_club_expenditure_few_year(start_year, end_year, team_num):
 
     return for_return_club_info
 
-team_num = 20
+team_num = 10
 print("Enter your want start year : ")
 start_year = input()
 print("Enter your want end year : ")
 end_year = input()
+print("Select League")
+print("0 : premier_league | 1 : serie_a | 2 : bundesliga | 3 : ligue_1 | 4 : laliga")
+league_num = input()
 
 # 원하는 시작 연도와 끝 연도를 입력 받고 함수 호출
 for i in range(int(start_year), int(end_year) + 1):
-    total_club_info = get_club_expenditure_few_year(i, i, team_num)
-    for i in range(0, 20):
+    total_club_info = get_club_expenditure_few_year(i, i, team_num, int(league_num))
+    for i in range(0, team_num):
         print(total_club_info[i])
     print()
