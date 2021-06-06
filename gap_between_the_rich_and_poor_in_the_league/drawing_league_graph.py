7b06325e00be3ce25d06cacc3074d9dcf722dfb9
MoonSeoJun <msj00130@gmail.com>
2021-06-06 22:37:23 +0900

    add : add select year interface

--- gap_between_the_rich_and_poor_in_the_league/drawing_league_graph.py
+++ gap_between_the_rich_and_poor_in_the_league/drawing_league_graph.py
@@ -4,27 +4,35 @@ import numpy as np
 from . import club_balance
 from . import survived_in_league
 
-def drawing_graph(league_arr):
+def drawing_graph(league_arr, start_year, end_year):
     labels = [league_arr[i][0] for i in range(0, len(league_arr))]
-    year_15 = [league_arr[i][1] for i in range(0, len(league_arr))]
-    year_16 = [league_arr[i][2] for i in range(0, len(league_arr))]
-    year_17 = [league_arr[i][3] for i in range(0, len(league_arr))]
-    year_18 = [league_arr[i][4] for i in range(0, len(league_arr))]
-    year_19 = [league_arr[i][5] for i in range(0, len(league_arr))]
-
-    c_bottom = np.add(year_15, year_16)
-    d_bottom = np.add(c_bottom, year_17)
-    f_bottom = np.add(d_bottom, year_18)
-
     x = range(len(labels))
-
     width = 0.5
+    count = 0
 
-    plt.bar(range(len(year_15)), year_15, width=width, label='2015/2016')
-    plt.bar(range(len(year_16)), year_16, bottom=year_15, width=width, label='2016/2017')
-    plt.bar(x, year_17, bottom=c_bottom, width=width, label='2017/2018')
-    plt.bar(x, year_18, bottom=d_bottom, width=width, label='2018/2019')
-    plt.bar(x, year_19, bottom=f_bottom, width=width, label='2019/2020')
+    for year in range(1, end_year - start_year + 1):
+        year_count = end_year - (end_year - start_year - year + 1)
+
+        current_bar = [league_arr[i][year] for i in range(0, len(league_arr))]
+
+        year_1 = [league_arr[i][year - 2] for i in range(0, len(league_arr))]
+        year_2 = [league_arr[i][year - 1] for i in range(0, len(league_arr))]
+        
+        if count == 0:
+            plt.bar(x, current_bar, width=width, label=f'{year_count}/{(year_count + 1)}')
+            count += 1
+        elif count == 1:
+            bottom_bar = [league_arr[i][year - 1] for i in range(0, len(league_arr))]
+            plt.bar(x, current_bar, bottom=bottom_bar, width=width, label=f'{year_count}/{(year_count + 1)}')
+            count += 1
+        elif count == 2:
+            add_arr = np.add(year_1, year_2)
+            plt.bar(x, current_bar, bottom=add_arr, width=width, label=f'{year_count}/{(year_count + 1)}')
+            count += 1
+        else:
+            add_arr = np.add(add_arr, year_2)
+            plt.bar(x, current_bar, bottom=add_arr, width=width, label=f'{year_count}/{(year_count + 1)}')
+
     plt.xticks(fontsize=7, rotation=45)
     plt.xlabel("Club name")
     plt.ylabel("Expenditure(million €)")
@@ -36,9 +44,9 @@ def drawing_graph(league_arr):
 
     plt.show()
 
-def arr_index_string_to_int(league_arr, survived_club):
+def arr_index_string_to_int(league_arr, survived_club, end_year):
     for i in range(0, len(survived_club)):
-        for j in range(1, 6):
+        for j in range(1, (end_year - start_year + 1)):
             new_string = ''.join(filter(str.isalnum, league_arr[i][j]))
             if len(new_string) == 6:
                 league_arr[i][j] = int(new_string[0:3])
@@ -49,15 +57,38 @@ def arr_index_string_to_int(league_arr, survived_club):
     
     return league_arr
 
-def call_drawing_function(league_num, survived_club):
-    league_arr = club_balance.print_total_club_info(league_num, survived_club)
-    #print(league_arr)
-    graph_arr = arr_index_string_to_int(league_arr, survived_club)
-    drawing_graph(graph_arr)
+def call_drawing_function(league_num, survived_club, start_year, team_num, end_year):
+    league_arr = club_balance.get_club_expenditure(league_num, survived_club, start_year, team_num, end_year)
+    graph_arr = arr_index_string_to_int(league_arr, survived_club, end_year)
+    drawing_graph(graph_arr, start_year, end_year)
 
 if __name__ == "__main__":
-    league = club_balance.bundesliga
-    start_year = 2015
-    team_num = 19 # Default - 21 |if you want to drawing Bundesliga expenditure graph, you change the number into 19
-    survived_club = survived_in_league.get_survived_clubs(league, start_year, team_num)
-    call_drawing_function(league, survived_club)
+    while True:
+        print("Select the league")
+        print("0 : Premier League | 1 : Serie A | 2 : Bundesliga | 3 : Ligue1 | 4 : La Liga")
+        league = int(input())
+
+        print("Input the start year")
+        start_year = int(input())
+
+        print("Input the end year")
+        end_year = int(input()) # if your select 2020, last season is 2019/2020
+
+        if start_year > end_year:
+            print("-----------------------------------------")
+            print("!Start year can not bigger than End year!")
+            print("Try Input again\n")
+        elif league < 0 or league > 4:
+            print("-----------------------------------------")
+            print("!This league number is not exist!")
+            print("Try Input again\n")
+        else:
+            break
+
+    if league == 2:
+        team_num = 19 # Exist club in Bundesliga are 18
+    else:
+        team_num = 21 # Exist club in other league are 20
+
+    survived_club = survived_in_league.get_survived_clubs(league, start_year, team_num, end_year)
+    call_drawing_function(league, survived_club, start_year, team_num, end_year)
